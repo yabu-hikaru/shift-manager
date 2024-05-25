d95854182a09d9b5fd8473d03adb2c14a77c0019
Hikaru Yabu <yabhika2507@gmail.com>
2024-05-25 21:42:32 +0900

    カレンダー表示_当月分

---
+++ src/utils/calendar.js
@@ -0,0 +1,43 @@
+import dayjs from "dayjs";
+import ja from "dayjs/locale/ja"
+
+dayjs.locale(ja)
+
+export const createCalender = ( month = dayjs().month(), year = dayjs().year() ) => {
+  const firstDateOfMonth = dayjs().year(year).month(month).startOf("month");
+  const lastDateOfMonth = dayjs().year(year).month(month).endOf("month");
+
+  const arrayOfDate = [];
+
+  //テーブル表示のための前月の取得
+  for( let i = 0; i < firstDateOfMonth.day(); i++ ) {
+    arrayOfDate.push({ 
+      date: firstDateOfMonth.day(i),
+      currentMonth: false,
+    });
+  }
+
+  //当月の取得
+  for( let i = firstDateOfMonth.date(); i <= lastDateOfMonth.date(); i++ ) {
+    arrayOfDate.push({
+      date: firstDateOfMonth.date(i),
+      currentMonth: true,
+      today: 
+        firstDateOfMonth.date(i).toDate().toDateString() === 
+        dayjs().toDate().toDateString(),
+    });
+  }
+
+  //5 * 7(5週分)のための35
+  const nextMonthDays = 35 - arrayOfDate.length
+
+  //テーブル表示のための翌月の取得
+  for ( let i = lastDateOfMonth.date() + 1; i <= lastDateOfMonth.date() + nextMonthDays; i++ ) {
+    arrayOfDate.push({
+      date: lastDateOfMonth.date(i),
+      currentMonth: false,
+    });
+  }
+
+  return arrayOfDate;
+}
